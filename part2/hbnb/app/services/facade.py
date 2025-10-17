fd69cc9e4dee385c2d80c68c30e7b88b49434398
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-10-17 16:21:35 +0000

    part_2_advanced

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -31,3 +31,18 @@ class HBnBFacade:
             setattr(user, key, value)
         self.user_repo.update(user_id, user)
         return user
+
+    def create_amenity(self, amenity_data):
+        from app.models.amenity import Amenity
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        return self.amenity_repo.update(amenity_id, amenity_data)
